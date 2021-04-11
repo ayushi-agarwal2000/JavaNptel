426c6fdaf70b8c079e0f5ec45e72d3c739c8f07b
Divya-2001 <60807380+Divya-2001@users.noreply.github.com>
2021-04-11 11:13:50 +0530

    Add files via upload

---
+++ Question1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Question1{
+	public static void main(String args[]){
+        int n;
+// write the code
+int sum=0;
+for(int i=2;i<=n*2;i+=2){
+  sum +=i;}
+System.out.print(sum);
+ } // main() ends here
+} // class Question1 ends here
